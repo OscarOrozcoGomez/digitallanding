8148a6ad605a0cd97fb56c424deec821085f8ab4
Oscar Orozco <allmithy@gmail.com>
2020-03-07 23:41:48 -0600

    moví la impresion de variables

--- mailer.php
+++ mailer.php
@@ -16,10 +16,6 @@
         if ( empty($name) OR empty($message) OR empty($phone) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo $name;
-            echo $email;
-            echo $phone;
-            echo $message;
             echo "Oops! Hubo algun problema con su envio. Por favor complete el formulario y intente de nuevo.";
             exit;
         }
@@ -44,7 +40,11 @@
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "TGracias! Su mensaje fue enviado con exito.";
+            echo $name;
+            echo $email;
+            echo $phone;
+            echo $message;
+            echo "Gracias! Su mensaje fue enviado con exito.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
